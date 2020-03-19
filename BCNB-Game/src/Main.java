4421c2318be25cdf667fccc682b183a93cc6f6a3
bragdonn <bragdonn@BY1928A75002349.wit.private>
2020-03-19 13:45:22 -0400

    Method Initialization.

--- BCNB-Game/src/Main.java
+++ BCNB-Game/src/Main.java
@@ -1,16 +1,67 @@
-import javafx.beans.binding.Bindings;
-import javafx.geometry.Point2D;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import java.io.File;
-import java.util.Map;
-public class Main {
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	private HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
+	
+	private ArrayList<Node> platforms = new ArrayList<Node>();
+	
+	private Pane appRoot = new Pane();
+	private Pane gameRoot = new Pane();
+	private Pane uiRoot = new Pane();
+	
+	private Node player;
+	private Point2D playerVelocity = new Point2D(0,0);
+	private boolean canJump = true;
+	
+	private int levelWidth;
+	
+	private void initContent(){
+		return;
+	}
+	
+	private void update(){
+		return;
+	}
+	
+	private void movePlayerX(int value){
+		return;
+	}
+	
+	private void movePlayerX(int value){
+		return;
+	}
+	
+	private void jumpPlayer(){
+		return;
+	}
+	
+	private Node createEntity(int x, int y, int w, int h, Color color){
+		return null;
+	}
+	
+	private boolean isPressed(KeyCode key){
+		return true;
+	}
+	
+	public void start(Stage primaryStage) throws Exception{
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub.
+		launch(args);
 
 	}
 
